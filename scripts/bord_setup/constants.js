fb90fc51062e6aca04a38b9d2985007227ee1775
Paliachi <a.bursulaia@gmail.com>
2023-05-25 18:43:35 +0200

    override of board initial setup

---
+++ scripts/bord_setup/constants.js
@@ -0,0 +1,24 @@
+export const FIGURES = {
+    "W_ROOK": {ID: 1, PATH: "./images/rook-w.svg"},
+    "W_KNIGHT": {ID: 2, PATH: "./images/king-w.svg"},
+    "W_BISHOP": {ID: 3, PATH: "./images/bishop-w.svg"},
+    "W_QUEEN": {ID: 4, PATH: "./images/queen-white.svg"},
+    "W_KING": {ID: 5, PATH: "./images/king-w.svg"},
+    "W_PAWN": {ID: 6, PATH: "./images/pawn-w.svg"},
+    "B_ROOK": {ID: 7, PATH: "./images/rook-b.svg"},
+    "B_KNIGHT": {ID: 8, PATH: "/images/archbis-b.svg"},
+    "B_BISHOP": {ID: 9, PATH: "/images/bishop-b.svg"},
+    "B_QUEEN": {ID: 10, PATH: "/images/queen-black.svg"},
+    "B_KING": {ID: 11, PATH: "/images/king-b.svg"},
+    "B_PAWN": {ID: 12, PATH: "/images/pawn-b.svg"},
+    "NONE": {ID: 0, PATH: ""}
+};
+
+
+
+//     "blackRook": "./images/rook-b.svg",
+//     "blackKnight": "/images/archbis-b.svg",
+//     "blackBishop": "/images/bishop-b.svg",
+//     "blackQueen": "/images/queen-black.svg",
+//     "blackKing": "/images/king-b.svg",
+//     "blackPawn": "/images/pawn-b.svg",
